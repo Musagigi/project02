cd38b05520f63a9c1bb42b6d2887063b0c819b3a
Musajiji <dzhal@bk.ru>
2024-09-17 18:37:04 +0300

    добавил ui button | a

--- src/shared/ui/buttons/Button/Button.tsx
+++ src/shared/ui/buttons/Button/Button.tsx
@@ -1,27 +1,41 @@
-import { ButtonHTMLAttributes } from 'react';
 import clsx from 'clsx';
+import { createElement } from 'react';
+
+import type { ComponentProps, ReactNode } from 'react';
 
 import styles from './Button.module.scss';
+
+type TValidTags = 'button' | 'a';
 
 type TButtonProps = {
   variant?: 'contained' | 'outlined' | 'text';
   size?: 'small' | 'medium' | 'large';
-} & ButtonHTMLAttributes<HTMLButtonElement>;
+  startIcon?: ReactNode;
+  endIcon?: ReactNode;
+  tag?: TValidTags;
+} & ComponentProps<TValidTags>;
 
 export const Button = ({
   variant = 'contained',
   size = 'medium',
+  startIcon,
+  endIcon,
+  tag = 'button',
   children,
   className,
   ...props
 }: TButtonProps) => {
-  return (
-    <button
-      {...props}
-      type="button"
-      className={clsx(styles.button, styles[variant], styles[size], className)}
-    >
-      <span>{children}</span>
-    </button>
+  // TODO кнопка и ссылка реализованы через один компонент
+  return createElement(
+    tag,
+    {
+      ...props,
+      className: clsx(styles.button, styles[variant], styles[size], className),
+    },
+    [
+      startIcon && <span key="startIcon">{startIcon}</span>,
+      <span key="children">{children}</span>,
+      endIcon && <span key="endIcon">{endIcon}</span>,
+    ],
   );
 };
