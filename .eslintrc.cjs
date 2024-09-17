9aabac77acec6d672c95b2299af8dddbcf0b2612
Musajiji <dzhal@bk.ru>
2024-09-17 18:45:21 +0300

    добавил stylelint, донастроил линтеры и конфиги

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,35 +1,57 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:prettier/recommended', 'plugin:storybook/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'plugin:storybook/recommended',
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+  ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh', 'simple-import-sort'],
+  plugins: ['react-refresh', "prettier"],
+  settings: {
+    "import/resolver": {
+      typescript: true,
+      node: true,
+    },
+  },
   rules: {
     'react/react-in-jsx-scope': 'off',
     'react-hooks/exhaustive-deps': 'off',
+    'prettier/prettier': 'error',
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true },
+      { allowConstantExport: false },
     ],
-    // "import/first": "error",
-    // "import/newline-after-import": "error",
-    // "import/no-duplicates": "error",
-    // 'simple-import-sort/imports': ['error', {
-    //   groups: [
-    //     ["^react", "^@?\\w"],
-    //     // Internal packages.
-    //     // ["^(@|@company|@ui|components|utils|config|vendored-lib)(/.*|$)"],
-    //     ["^"],
-    //     // Side effect imports.
-    //     ["^\\u0000"],
-    //     // Parent imports. Put `..` last.
-    //     ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
-    //     // Other relative imports. Put same-folder imports and `.` last.
-    //     ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
-    //     // Style imports.
-    //     ["^.+\\.s?css$"],
-    //   ]
-    // }]
+    'import/no-unresolved': 'error',
+    'import/no-duplicates': 'error',
+    'import/no-named-as-default-member': 'off',
+    'import/no-named-as-default': 'off',
+    "import/default": "off",
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'external',
+          'builtin',
+          'internal',
+          'index',
+          'type',
+          'parent',
+          'sibling',
+        ],
+
+        alphabetize: {
+          'order': 'asc',
+          'caseInsensitive': true
+        },
+        'newlines-between': 'always-and-inside-groups'
+      }
+    ],
   },
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import autoprefixer from 'autoprefixer';
+import { defineConfig } from 'vite';
+
 import path from 'path';
 
 type ViteConfig = {
